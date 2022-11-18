33efea6cf3a5d5494a70655bdd2b15287a75b619
skitnay <sima9729@gmail.com>
2022-11-18 18:25:51 +0300

    Prettier

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://skitnay.ru">SkitNay.ru</a>
-        </h1>
-      </main>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					Welcome to <a href="https://skitnay.ru">SkitNay.ru</a>
+				</h1>
+			</main>
+		</div>
+	);
 }
